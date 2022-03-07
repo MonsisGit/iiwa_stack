64a4cd3dde6115160720d6d9e6f0477309aa51df
MonsisGit <simon.goldhofer@gmail.com>
2022-03-07 13:36:47 +0100

    rem vision cpp

--- realtime/src/robot_control.cpp
+++ realtime/src/robot_control.cpp
@@ -174,3 +174,31 @@ int RobotControl::set_joint_positions(float j1, float j2, float j3, float j4, fl
 
   return 0;
 }
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "RobotControl");
+  RobotControl robot_control;
+
+  ros::Rate loop_rate(0.4);
+  ros::Duration(1).sleep();
+  int count = 1;
+
+  while (ros::ok())
+  {
+    if (count % 2 == 0)
+    {
+      robot_control.set_cartesian_pose(0.2, 0.1, 1, 0, 0, 0, 1);
+    }
+    else
+    {
+      robot_control.set_cartesian_pose(0.2, 0.0, 0.9, 0, 0, 0, 1);
+    }
+
+    count++;
+    loop_rate.sleep();
+    ros::spinOnce();
+  }
+
+  return 0;
+}
--- realtime/src/vision_control.cpp
+++
@@ -1,2 +0,0 @@
-#include "../include/vision_control.h"
-
